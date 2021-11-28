904a704169f719163132ac62ecbf8d87587a1f65
accounting-fit <towhi.japan@gmail.com>
2021-11-28 22:39:34 +0600

    Add project files.

---
+++ CRMWebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMWebApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("/Views/Main/Authentication/Login.cshtml");
+        }
+        public IActionResult Password()
+        {
+            return View("/Views/Main/Authentication/Password.cshtml");
+        }
+        public IActionResult Register()
+        {
+            return View("/Views/Main/Authentication/Register.cshtml");
+        }
+    }
+}
